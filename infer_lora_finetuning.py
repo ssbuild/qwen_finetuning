81e1cd9c479155891cd9fa294a97e3e06a69aea3
ssbuild <462304@qq.com>
2023-08-03 23:57:56 +0800

    remove
    Signed-off-by: ssbuild <462304@qq.com>

--- infer_lora_finetuning.py
+++ infer_lora_finetuning.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     
     lora_args = LoraArguments.from_pretrained(ckpt_dir)
 
-    assert lora_args.inference_mode == True and config.pre_seq_len is None
+    assert lora_args.inference_mode == True
 
     # new_num_tokens = config.vocab_size
     # if config.task_specific_params is not None and config.task_specific_params.get('vocab_size', None) is not None:
